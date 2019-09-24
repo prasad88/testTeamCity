b8b44225e39281426c64c6c82b1d61049e2c9aaf
Prasad Kutarekar <prasad.kutarekar@gmail.com>
2019-09-24 15:32:09 +0530

    Add files via upload

---
+++ src/main/java/com/example/demoForTeamCity/DemoForTeamCityApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoForTeamCity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoForTeamCityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoForTeamCityApplication.class, args);
+	}
+
+}
